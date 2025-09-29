ba624f6799b205124743c5da24fc8b4ebe9a90b4
eunkyoung <eunkyoung4160@gmail.com>
2025-09-29 21:50:06 +0900

    section5 : 스프링 빈과 의존관계

--- lecture01/hello-spring/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ lecture01/hello-spring/hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -8,8 +8,7 @@ import java.util.Optional;
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
-    Optional<Member> findByNmae(String name);
+    Optional<Member> findByName(String name);
     List<Member> findAll();
 
-    Optional<Member> findByName(String name);
 }
